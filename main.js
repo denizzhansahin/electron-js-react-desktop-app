308abee7b73f8fd71ce521b209b92dd0d2711e17
Denizhan Şahin <denizhansahin@yaani.com>
2025-01-04 22:18:05 +0300

    first commit

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-import { app, BrowserWindow } from "electron";
 
+
+const {app,BrowserWindow} = require("electron");
+const path = require("path");
+const url = require("url");
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: "Electron Denizhan",
+        width:1000,
+        height:600,
+    })
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
+    })
+
+
+    mainWindow.loadURL(startUrl);
+}
+
+
+
+app.whenReady().then(createMainWindow);
